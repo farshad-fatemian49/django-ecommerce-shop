cfc6b6ce7a20329dd4f5768699ba43e88298aff1
farshad fatemian <farshad.fatemian24@gmail.com>
2025-12-09 13:44:23 +0330

    Add files via upload

---
+++ My Site/accounts/models.py
@@ -0,0 +1,13 @@
+# from django.db import models
+# from django.contrib.auth.models import AbstractUser , AbstractBaseUser
+
+# # Create your models here.
+
+# class CustomUser(AbstractUser):
+#     age = models.PositiveIntegerField(null=True,blank=True)
+# #     class Meta:
+# #         model = User
+# #         fields = [
+# # 'username', 'email', 'first_name', 'last_name', 'password1', 'password2', 'check',
+# # ]
+    
